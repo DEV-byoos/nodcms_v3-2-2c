9d5d57c243b10c6170472cbe03527f74b83cb5ca
BYOOS <gbobard@gmail.com>
2022-06-23 11:39:39 +0100

    Update Database.php

--- nodcms-core/Config/Database.php
+++ nodcms-core/Config/Database.php
@@ -41,9 +41,9 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'byoosadmin',
-		'password' => 'sqlMamayo!237',
-		'database' => 'v2022_nodcms_v3_2_2c _dev',
+		'username' => '',
+		'password' => '',
+		'database' => '',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
